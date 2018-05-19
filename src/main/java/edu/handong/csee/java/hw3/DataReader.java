95cab75dd553c66601212597f692ede501a160ff
otterlee <21700525@handong.edu>
2018-05-19 14:24:44 +0900

    Reading Data first version

--- src/main/java/edu/handong/csee/java/hw3/DataReader.java
+++ src/main/java/edu/handong/csee/java/hw3/DataReader.java
@@ -1,5 +1,60 @@
 package edu.handong.csee.java.hw3;
 
-public class DataReader {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class DataReader {
+	
+	public static void main(String[] args){
+		DataReader dataReader = new DataReader();
+		dataReader.getData(args[0]);
+	}
+
+	public ArrayList<String> getData(String strDir){
+		//(1) getDirectory
+		File myDir = getDirectory(strDir);
+		//(2) getListOfFilesFromDirectory
+		File[] files = getListOfFilesFromDirectory(myDir);
+		//(3)
+		ArrayList<String> messages = readFiles(files);
+
+		return messages;
+	}
+
+	private File getDirectory(String strDir) {
+		File myDirectory = new File(strDir);
+		System.out.println("myDirectory : " +myDirectory);
+		return myDirectory;
+
+	}
+
+	private File[] getListOfFilesFromDirectory(File dataDir) {
+		for(File file: dataDir.listFiles()) {
+			System.out.println("->"+file.getAbsolutePath());
+		}
+		return dataDir.listFiles();
+	}
+
+	private ArrayList<String> readFiles(File[] files) {
+		ArrayList<String> messages = new ArrayList<String>();
+		Scanner inputStream = null;
+		for(File f :files) {
+				try{
+					inputStream = new Scanner(f);
+				} catch (FileNotFoundException e) {
+					System.out.println ("Error opening the file ");
+					System.exit(0);
+				}
+				while (inputStream.hasNextLine ()) {
+					String line = inputStream.nextLine ();
+					messages.add(line);
+					System.out.println (line);
+				}
+			}
+		inputStream.close();
+		return messages;
+	}
 }
+
