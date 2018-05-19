2c84cd0dd94bd55b6e689db089ee61f47c31e117
otterlee <21700525@handong.edu>
2018-05-19 20:18:25 +0900

    Minor change

--- src/main/java/edu/handong/csee/java/hw3/ChatMessageCounter.java
+++ src/main/java/edu/handong/csee/java/hw3/ChatMessageCounter.java
@@ -1,10 +1,12 @@
 package edu.handong.csee.java.hw3;
 
 public class ChatMessageCounter {
-	
+	HashMap<String, Integer> chatDataList;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//DataReader(여기서 csv랑 txt를 비교한다.)
+		//MessageFilter
+		//DataWriter
 	}
 
 }
